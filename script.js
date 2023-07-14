0256868c9ff61de77a118e9ffca3950874b840c7
Nagasai <nagasai36912@gmail.com>
2023-07-14 21:52:11 +0530

    text basic complete

--- script.js
+++ script.js
@@ -1,72 +1,172 @@
-const encrypt = document.getElementById("encrypt-text");
-encrypt.addEventListener("click", encryptf);
+//selection
 
-const decrypt = document.getElementById("decrypt-text");
-decrypt.addEventListener("click", decryptf);
+checkBoxText = document.getElementById("checkbox-text");
+checkBoxText.addEventListener("click", function() {
+    checkBoxImg.checked = false;
+    dropImg.disabled = true;
+    dropText.disabled = false;
 
-const reset = document.getElementById("reset-text");
-reset.addEventListener("click", resetf);
+});
 
+checkBoxImg = document.getElementById("checkbox-img");
+checkBoxImg.addEventListener("click", function() {
+    checkBoxText.checked = false;
+    dropText.disabled = true;
+    dropImg.disabled = false;
+});
 
-
-
-
-
-
-
-/*functions*/
-
-function padding(str, n) {
-    let pad = n-str.length;
-    for(let i=0; i<pad; i++) {
-        str = '0'+str;
+dropText = document.getElementById("drop-text");
+dropText.addEventListener("change", function() {
+    let list = document.getElementsByClassName("try-subcontainer")
+    for(let i=0; i<list.length; i++) {
+        list[i].style.display = "none";
     }
-    return str;
-}
+    if(dropText.value != "none") {
+        document.getElementById(dropText.value).style.display = "block";
+    }
+});
 
-function encryptf() {
-    let input = document.getElementById("text-input").value;
-    let key = Math.floor(Math.random()*1000)%128;
-    let keyBinary = padding(key.toString(2),7);
-    let inputBinary = "";
+dropImg = document.getElementById("drop-img");
+dropImg.addEventListener("change", function() {
+    let list = document.getElementsByClassName("try-subcontainer");
+    for(let i=0; i<list.length; i++) {
+        list[i].style.display = "none";
+    }
+    if(dropImg.value != "none") {
+        document.getElementById(dropImg.value).style.display = "block";
+    }
+});
+
+
+//try-noKey
+
+tryNoKeyEncryptB = document.getElementById("try-noKey-encrypt");
+tryNoKeyEncryptB.addEventListener("click", function () {                    // There are two keys which are generated randomly key0 and key1 resp
+    input = document.getElementById("try-no-key-fields-plaintext").value;   //we encrypt unicode values as x -> (x+key0 or x+key1)
+    let key0 = (Math.floor(Math.random()*1000)%61)+161;                     // depending on index of x 
+    let key1 = (Math.floor(Math.random()*1000)%61)+161;                     // after encrypting the original string 
+    output = "";                                                            // we concatenate "key0" + "encyrpted" + "key1"
+    output += String.fromCharCode(key0);                                    // this is our output
+    for(let i=0; i<input.length; i++) {                                     // note that keys are always in range [161,221]
+        if(i%3==0) {
+            output += String.fromCharCode(input.charCodeAt(i)+key0);
+        }
+        else {
+            output += String.fromCharCode(input.charCodeAt(i)+key1);
+        }
+    }
+    output += String.fromCharCode(key1);
+
+    document.getElementById("try-no-key-fields-ciphertext").value = output;
+
+});
+
+tryNoKeyDecryptB = document.getElementById("try-noKey-decrypt");
+tryNoKeyDecryptB.addEventListener("click", function () {                        // same as above but reverse
+    let input = document.getElementById("try-no-key-fields-ciphertext").value;
+    let key0 = input.charCodeAt(0);
+    let key1 = input.charCodeAt(input.length-1);
+    input = input.substring(1,input.length-1);
+    output = "";
     for(let i=0; i<input.length; i++) {
-        inputBinary += padding((input.charCodeAt(i)^key).toString(2),7);
+        if(i%3 == 0) {
+            output += String.fromCharCode(input.charCodeAt(i)-key0);
+        }
+        else {
+            output += String.fromCharCode(input.charCodeAt(i)-key1);
+        }
     }
-    let outputBinary = "";
-    outputBinary += keyBinary.substring(0,4);
-    outputBinary += inputBinary;
-    outputBinary += keyBinary.substring(4);
-    
-    let output = "";
-    for(let i=0; i<(outputBinary.length/7); i++) {
-        let character = outputBinary.substring(7*i, 7*i+7);
-        output += String.fromCharCode(parseInt(character,2));
-    }
+    document.getElementById("try-no-key-fields-plaintext").value = output;
+});
 
-    document.getElementById("text-output").value = output;
-    console.log("key is: " + key);
-}
+tryNoKeyResetB = document.getElementById("try-noKey-reset");
+tryNoKeyResetB.addEventListener("click", function () {
+    document.getElementById("try-no-key-fields-plaintext").value = "";
+    document.getElementById("try-no-key-fields-ciphertext").value = "";
+});
 
-function decryptf() {
-    input = document.getElementById("text-output").value;
-    inputBinary = "";
+//try-sysGenKey
+
+trySysGenKeyEncryptB = document.getElementById("try-sysGenKey-encrypt");
+trySysGenKeyEncryptB.addEventListener("click", function () {
+    input = document.getElementById("try-sysGenKey-fields-plaintext").value;
+    let key0 = (Math.floor(Math.random()*1000)%61)+161;
+    let key1 = (Math.floor(Math.random()*1000)%61)+161;
+    output = "";
     for(let i=0; i<input.length; i++) {
-        inputBinary += padding(input.charCodeAt(i).toString(2), 7);
+        if(i%3==0) {
+            output += String.fromCharCode(input.charCodeAt(i)+key0);
+        }
+        else {
+            output += String.fromCharCode(input.charCodeAt(i)+key1);
+        }
     }
 
-    let keyBinary = inputBinary.substring(0,4)+inputBinary.substring(inputBinary.length-3);
-    let key = parseInt(keyBinary, 2);
-    let outputBinary = inputBinary.substring(4,inputBinary.length-3);
-    let output = "";
-    for(let i=0; i<(outputBinary.length/7); i++) {
-        let character = outputBinary.substring(7*i, 7*i+7);
-        output += String.fromCharCode(parseInt(character,2)^key);
-    }
-    
-    document.getElementById("text-input").value = output;
-}
+    document.getElementById("try-sysGenKey-fields-ciphertext").value = output;
+    document.getElementById("try-sysGenKey-fields-key").value = key0.toString() + key1.toString();
+});
 
-function resetf() {
-    document.getElementById("text-input").value = "";
-    document.getElementById("text-output").value = "";
-}
+trySysGenKeyDecryptB = document.getElementById("try-sysGenKey-decrypt");
+trySysGenKeyDecryptB.addEventListener("click", function () {
+    input = document.getElementById("try-sysGenKey-fields-ciphertext").value;
+    let keys = document.getElementById("try-sysGenKey-fields-key").value;
+    let key0 = parseInt(keys.substring(0,3),10);
+    let key1 = parseInt(keys.substring(3), 10);
+    output = "";
+    for(let i=0; i<input.length; i++) {
+        if(i%3 == 0) {
+            output += String.fromCharCode(input.charCodeAt(i)-key0);
+        }
+        else {
+            output += String.fromCharCode(input.charCodeAt(i)-key1);
+        }
+    }
+
+    document.getElementById("try-sysGenKey-fields-plaintext").value = output;
+});
+
+trySysGenKeyResetB = document.getElementById("try-sysGenKey-reset");
+trySysGenKeyResetB.addEventListener("click", function () {
+    document.getElementById("try-sysGenKey-fields-ciphertext").value = "";
+    document.getElementById("try-sysGenKey-fields-key").value = "";
+    document.getElementById("try-sysGenKey-fields-plaintext").value = "";
+});
+
+// try-userGenKey
+
+tryUserGenKeyEncryptB = document.getElementById("try-userGenKey-encrypt");
+tryUserGenKeyEncryptB.addEventListener("click", function () {
+    let input = document.getElementById("try-userGenKey-fields-plaintext").value;                // key is num <= 60000
+    let key = parseInt(document.getElementById("try-userGenKey-fields-key").value,10)+161161;   // this is chosen so that key will be between 
+    let key0 = parseInt(key.toString().substring(0,3),10);                                                   // 161161 and 221221
+    let key1 = parseInt(key.toString().substring(3),10);                                                     // consider this num as key0 + key1
+    output = "";
+    for(let i=0; i<input.length; i++) {
+        if(i%3 == 0) {
+            output += String.fromCharCode(input.charCodeAt(i)+key0);
+        }
+        else {
+            output += String.fromCharCode(input.charCodeAt(i)+key1);
+        }
+    }
+
+    document.getElementById("try-userGenKey-fields-ciphertext").value = output;
+});
+
+tryUserGenKeyDecryptB = document.getElementById("try-userGenKey-decrypt");
+tryUserGenKeyDecryptB.addEventListener("click", function () {
+    let input = document.getElementById("try-userGenKey-fields-ciphertext").value;
+    let key = parseInt(document.getElementById("try-userGenKey-fields-key").value,10)+161161;
+    let key0 = parseInt(key.toString().substring(0,3),10);                                                   // 161161 and 221221
+    let key1 = parseInt(key.toString().substring(3),10);  
+    output = "";
+    for(let i=0; i<input.length; i++) {
+        if(i%3 == 0) {
+            output += String.fromCharCode(input.charCodeAt(i)-key0);
+        }
+        else {
+            output += String.fromCharCode(input.charCodeAt(i)-key1);
+        }
+    }
+    document.getElementById("try-userGenKey-fields-plaintext").value = output;
+});
